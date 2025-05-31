3d3607c6245b8d1d0d6a05f3dbe054a26fcd46d5
Murod's Macbook <murodlearns@gmail.com>
2025-06-01 05:05:54 +0900

    fix: modify notice service related graphql api business logic

--- apps/nestar-api/src/libs/enums/notice.enum.ts
+++ apps/nestar-api/src/libs/enums/notice.enum.ts
@@ -3,7 +3,7 @@ import { registerEnumType } from '@nestjs/graphql';
 export enum NoticeCategory {
 	FAQ = 'FAQ',
 	TERMS = 'TERMS',
-	INQUIRY = 'INQUIRY',
+	NOTICE = 'NOTICE',
 }
 registerEnumType(NoticeCategory, {
 	name: 'NoticeCategory',
